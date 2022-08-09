e52fe7613d6b47bf11b637659574cd3813e27141
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-09 16:00:22 -0400

    Create randomNumber function

---
+++ script.js
@@ -0,0 +1,5 @@
+//Random number generator will be used as a way to generate the meme when a button is clicked
+let randomNumber = () => {
+    let randNum = Math.floor(Math.random()*10);
+    return randNum;
+}
