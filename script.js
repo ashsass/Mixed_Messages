26173bc6aef90e619cf6360c22b2b77a56847110
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-09 16:01:16 -0400

    Create array to hold content

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let randomNumber = () => {
     let randNum = Math.floor(Math.random()*10);
     return randNum;
 }
+
+//Arrays that will hold the memes content. Using strings for now until I can figure out how to use media
+let emptyArray = [];
+
+let memeHolder = {
+    evilMeme: ['chaosCat1'],
+    neutralMeme: ['neutralCat1', 'neutralCat2', 'neutralCat3'],
+    goodMeme: ['goodCat1', 'goodCat2', 'goodCat3'],
+}
