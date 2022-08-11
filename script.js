326b62cd87c6a80e1c8170056d63ad6d74c0323e
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-11 14:34:47 -0400

    Create variables to hold images in js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ let memeHolder = {
     neutralMeme: ['neutralCat1', 'neutralCat2', 'neutralCat3'],
     goodMeme: ['goodCat1', 'goodCat2', 'goodCat3'],
 }
+
+//Variables for images
+let goodMeme1 = document.getElementById('good-meme-one');
+let goodMeme2 = document.getElementById('good-meme-two');
+let goodMeme3 = document.getElementById('good-meme-three');
+
+let neutralMeme1 = document.getElementById('neutral-meme-one');
+let neutralMeme2 = document.getElementById('neutral-meme-two');
+let neutralMeme3 = document.getElementById('neutral-meme-three');
+
+let evilMeme1 = document.getElementById('evil-meme-one');
+let evilMeme2 = document.getElementById('evil-meme-two');
+let evilMeme3 = document.getElementById('evil-meme-three');
