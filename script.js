f644208b1d09971b971b4b362025fcde77dee341
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-19 12:36:29 -0400

    New image appears when button is clicked instead of adding a new image below the current one

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Button variable
+const button = document.getElementById('button');
+
 //Array to hold urls
 const urlArray = ['./resources/photos/goodImage1.jpeg', './resources/photos/goodImage2.jpeg', './resources/photos/goodImage3.jpeg', './resources/photos/neutralImage1.jpeg', './resources/photos/neutralImage2.jpeg', './resources/photos/neutralImage3.jpeg', './resources/photos/evilImage1.jpeg', './resources/photos/evilImage2.jpeg', './resources/photos/evilImage3.jpeg'];
 
@@ -9,11 +12,10 @@ const getRandomData = arr => arr[randomNumber(arr.length)];
 
 const showMeme = () => {
     //Create a random number and set it as the index holding the meme
-    let randomNumber = randomNumber();
-    const randomUrl = urlArray[randomNumber];
+    const randomUrl = getRandomData(urlArray);
 
     //Create a variable to represent that place where the meme will appear
-    const container = document.getElementsByClassName('picture-container');
+    const container = document.querySelector('.picture-container');
 
     //Create the newImage variable to hold the meme
     const newImage = document.createElement('img');
@@ -21,6 +23,15 @@ const showMeme = () => {
     //Use setAttribute to give it a src and then the randomUrl as the contents
     newImage.setAttribute('src', randomUrl);
 
+    //Check if img is already present and then clear it. This will help reset the image everytime the button is clicked
+    let isImg = container.querySelector('img');
+
+    if(isImg){
+        container.innerHTML = '';
+    }
+
     //Add it to the document
     container.appendChild(newImage);
 }
+
+button.addEventListener('click', showMeme);
